a12eccb850436f0ca3b80098262961858093b40b
Christy O'Reilly <christy@oreills.co.uk>
2012-11-13 09:06:09 +0000

    Basic gif roulette

--- roulette.py
+++ roulette.py
@@ -1,13 +1,29 @@
-from random import choice
+from random import choice, uniform
+import PIL
 
-ROULETTE_LEN = 40
-ANIMATION_LEN = 5 # seconds
+from images2gif import writeGif
+
+ROULETTE_ITEMS = 40
+ANIMATION_DURATION = 10
+DAMPING_FACTOR = 1.2
+
+def _calculate_durations(items, damping_factor):
+    durations = tuple(reversed([1/(float(DAMPING_FACTOR)**n) for n in xrange(items)]))
+    return durations, sum(durations)
+
+DURATIONS, TOTAL_DURATIONS = _calculate_durations(ROULETTE_ITEMS, DAMPING_FACTOR)
 
 EMOTE_CACHE = {}
 WORD_CACHE = {}
 
+
 def rand_seq(choices, n):
     return [choice(choices) for _ in xrange(n)]
+
+def test_roulette():
+    from glob import glob
+    image_filenames = glob('image_cache/*.gif')
+    gif_roulette(image_filenames)
 
 def gif_roulette(tokens):
     return _generic_roulette(tokens, get_emote_image)
@@ -16,16 +32,23 @@ def word_roulette(tokens):
     return _generic_roulette(tokens, get_word_image)
 
 def _generic_roulette(tokens, get_image_fn):
-    roulette_seq = rand_seq(tokens, ROULETTE_LEN)
+    roulette_seq = rand_seq(tokens, ROULETTE_ITEMS)
     images = [get_image_fn(s) for s in roulette_seq]
     roulette_image = roulettify_images(images)
     return roulette_image
 
+def random_scaled_durations():
+    anim_duration = ANIMATION_DURATION * uniform(0.8, 1.2)
+    scale = anim_duration/TOTAL_DURATIONS
+    return tuple(d * scale for d in DURATIONS)
+
 def roulettify_images(images):
-    pass
+    durations = random_scaled_durations()
+    roulette_image = writeGif('out/out.gif', images, durations)
+    return roulette_image
 
 def get_emote_image(emote):
-    pass
+    return PIL.Image.open(emote)
 
 def get_word_image(word):
     pass
